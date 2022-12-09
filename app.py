e150c4f55f81738dab65d9175ba053b139d085ea
arukaen <arukaen@users.noreply.github.com>
2022-12-08 19:19:57 -0500

    logging

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+logger = logging.getLogger("fastapi")
+
 class Message(BaseModel):
     message: str
 
-@app.get("/")
-def hello():
+@app.post("/")
+def get_message(message: Message):
+    logger.info("Received message: %s", message.message)
     return Message(message="Hello World")
