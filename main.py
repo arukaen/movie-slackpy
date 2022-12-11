fefa6cbef07429bfdb865d2c79c25981354c072c
arukaen <arukaen@users.noreply.github.com>
2022-12-11 17:04:26 -0500

    updates

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from urllib.parse import parse_qs
+from urllib.parse import parse_qsl
+import requests
+import json
+import os
+
+bot_name = os.environ.get('BOT_NAME', 'Steve')
+emoji = os.environ.get('EMOJI', ':movie_camera:')
+omdb_key = os.environ.get('OMDB_API_KEY')
+omdb_url = 'https://www.omdbapi.com/'
+slack_webhook_url = os.environ.get('')
+
+def omdb_query(movie):
+    payload = {'apikey': omdb_key, 't': movie}
+    response = requests.get(omdb_url, params=payload)
+    result = json.loads(response.txt)
+    return result
 
 def handler(event, context):
   # Ensure the request method is POST
@@ -8,16 +23,63 @@ def handler(event, context):
       'body': 'Method Not Allowed',
     }
 
-  # Parse the form data
-  body = parse_qs(event['body'])
-  
-  for k, v in body.items():
-      print(f'{k}: {v}')
+  # Ensure the request content-type is "application/x-www-form-urlencoded"
+  if event['headers']['content-type'] != 'application/x-www-form-urlencoded':
+    return {
+      'statusCode': 400,
+      'body': 'Bad Request',
+    }
 
-  # Do something with the form data
+  # Parse the form data
+  body = event['body']
+  parsed = dict(parse_qsl(body))
+
+  search_term = parsed["text"]
+  channel_id = parsed["channel_id"]
+
+  results = omdb_query(search_term)
+
+  response_data = {
+        "username": bot_name
+        "icon_emoji": emoji,
+        "channel": channel_id,
+        "attachments": [{
+            "title": results["Title"],
+            "title_link": "https://www.imdb.com/title/" + results["imdbID"],
+            "color": "#7B00FF",
+            "image_url": results["Poster"],
+            "fields": [{
+                "title": "Rating",
+                "value": rating,
+                "short": True,
+            },
+            {
+                "title": "Year",
+                "value": results["Year"],
+                "short": True,
+            },
+            {
+                "title": "Runtime",
+                "value": results["Runtime"],
+                "short": True,
+            },
+            {
+                "title": "Director",
+                "value": results["Director"],
+                "short": True,
+            },
+            {
+                "title": "Actors",
+                "value": results["Actors"],
+            }]
+        }]
+    }
+
+  return_content = requests.post(slack_webhook_url, json=response_data, headers={'Content-Type': 'application/json'})
+
 
   # Return a success response
   return {
     'statusCode': 200,
-    'body': 'Success',
+    'body': '',
   }
